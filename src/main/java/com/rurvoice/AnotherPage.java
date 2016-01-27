a19f9d12aa379b2ff7a0f8473ee079210df18569
乐为 lewei <lewei.me@gmail.com>
2016-01-27 11:02:44 +0800

    add jump test

---
+++ src/main/java/com/rurvoice/AnotherPage.java
@@ -0,0 +1,20 @@
+package com.rurvoice;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+public class AnotherPage extends WebPage {
+
+	public AnotherPage(final PageParameters pageParameters) {
+		super(pageParameters);
+		add(new Link("linkToHomePage") {
+			@Override
+			public void onClick() {
+				// TODO Auto-generated method stub
+				setResponsePage(HomePage.class);
+			}
+		});
+	}
+
+}
--- src/main/java/com/rurvoice/HomePage.java
+++ src/main/java/com/rurvoice/HomePage.java
@@ -2,6 +2,7 @@ package com.rurvoice;
 
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.WebPage;
 
 public class HomePage extends WebPage {
@@ -13,8 +14,15 @@ public class HomePage extends WebPage {
 		add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
 
 		// TODO Add your page's components here
-		
-		add(new Label("helloWorld", "Hello World!"));
 
-    }
+		add(new Label("helloWorld", "Hello World!"));
+		
+		add(new Link("linkToAnotherPage") {
+			@Override
+			public void onClick() {
+				// TODO Auto-generated method stub
+				setResponsePage(AnotherPage.class);
+			}
+		});
+	}
 }
